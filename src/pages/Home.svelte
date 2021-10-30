28d899f0d960d9a82e3973673c885df8763443d7
Kanik Goyal <acrotechllp@gmail.com>
2021-10-30 10:59:01 +0530

    first

---
+++ src/pages/Home.svelte
@@ -0,0 +1,103 @@
+<style>
+    .aa{
+        margin-top: 110px;
+    }
+    .bb{
+        height: 70px;
+    }
+    .cc{
+        color: black;
+    }
+    .dd{
+        text-align: center;
+    }
+    .ee{
+        color: yellow;
+    }
+    .ff{
+        width: 100vw;
+    }
+   
+   
+</style>
+<div class="table-responsive aa ff">
+    <table class="table">
+        <thead>
+            <tr>
+                <th class="cc">Quantity</th>
+                <th class="cc dd">(req.)</th>
+                <th class="cc dd">1800</th>
+                <th class="cc dd">945</th>
+                <th class="cc dd">Report</th>
+                <th class="cc dd">Short</th>
+                
+            </tr>
+        </thead>
+        <thead>
+            <tr>
+                <th class="cc">Elements</th>
+                <th class="cc dd">%</th>
+                <th class="cc dd">K.g</th>
+                <th class="cc dd">K.g</th>
+                <th class="cc">%</th>
+                <th class="cc dd">K.g</th>
+                
+            </tr>
+        </thead>
+        <tbody class="table-dark">
+            <tr class="bb dd ff">
+                <td>Ni</td>
+                <td>2</td>
+                <td>2400</td>
+                <td>120</td>
+                <td class="ee">1.4</td>
+                <td>120</td>
+                
+            </tr>
+            <tr class="bb dd">
+                <td>Cr</td>
+                <td>12.5</td>
+                <td>340</td>
+                <td>10</td>
+                <td class="ee">5</td>
+                <td>10</td>
+               
+            </tr>
+            <tr class="bb dd">
+                <td>Mn</td>
+                <td>9</td>
+                <td>1400</td>   
+                <td>4.5</td>
+                <td class="ee">900</td>
+                 <td>4.5</td>
+               
+            </tr>
+            <tr class="bb dd">
+                <td>Cu</td>
+                <td>2</td>
+                <td>150</td>
+                <td>.75</td>
+                <td class="ee">60</td>
+                <td>.75</td>
+            </tr>
+            <tr class="bb dd">
+                <td>C</td>
+                <td>.16</td>
+                <td>2.4</td>
+                <td>2.1</td>
+                <td class="ee">3.5</td>
+                <td>2.1</td>
+                
+            </tr>
+            <tr class="bb dd">
+                <td>Others</td>
+                <td>75</td>
+                <td>1500</td>
+                <td>1000</td>
+                <td class="ee">73</td>
+                <td>500</td>
+               
+            </tr>
+        </tbody>
+    </table>
+</div>
---
+++ src/pages/Sample.svelte
@@ -0,0 +1,56 @@
+<style>
+.aa{
+    display: flex;
+    flex-direction: column;
+    margin-top: 190px;
+}
+.bb{
+    height: 25vh;
+    display: flex;
+    flex-wrap: wrap;
+
+}
+.cc{
+    height: 35px;
+    width: 100px;
+    margin-right: 20px;
+}
+.dd{
+    margin-right: 30px;
+    height: 35px;
+    padding-top: 5px;
+    color: white;
+}
+.ee{
+    background-color: #212529;
+    width: 100vw;
+}
+.ff{
+    margin-top: 10px;
+}
+.zz{
+    margin-right: 90px;
+    margin-left: 32px;
+}
+
+.ii{
+    margin-top: 50px;
+    width: 100px;
+    margin-left: 37%;
+}
+</style>
+<div class="aa">
+<div class="bb ee">
+    <h4 class="dd ff zz">Sample Heat</h4>
+    <input class="w3-input w3-border cc w3-round ff" type="number" value="0" >
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="Ni" >
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="Cr">
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="Mn">
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="Cu">
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="C">
+    <input class="w3-input w3-border cc w3-round ff" type="number"  placeholder="Others">   
+</div>
+
+<button type="button" class="btn btn-warning ii">Apply</button>
+
+</div>
